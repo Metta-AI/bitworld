413bc9689fc34f2e043a95840c473b11f01d062e
malcolm handley <m@redheron.com>
2026-05-04 23:44:18 +0000

    Support log extraction on antfarm/cloudflare
    Antfarm overrides the docker entrypoint to use curl to post each line of output as it happens

--- flake.nix
+++ flake.nix
@@ -184,6 +184,7 @@
             bitworldAmongThem
             pkgs.bashInteractive
             pkgs.coreutils
+            pkgs.curl # Used to extract logs at runtime in cloudflare
             pkgs.tini
           ] ++ runtimeLibs;
           config = {
@@ -206,6 +207,7 @@
             bitworldNottoodumb
             pkgs.bashInteractive
             pkgs.coreutils
+            pkgs.curl # Used to extract logs at runtime in cloudflare
             pkgs.tini
             pkgs.cacert # bots dial out over wss://, need a trust store
           ] ++ runtimeLibs;
